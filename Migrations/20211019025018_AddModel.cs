52907ac61562689fcebdbe6acf3db59d5e7dde47
Juan daniel Gonzales Aramayo <a2020116081@estudiantes.upsa.edu.bo>
2021-10-18 22:50:56 -0400

    Migrer los modelos hacia la base de datos

---
+++ Migrations/20211019025018_AddModel.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WebApiSuerte.Migrations
+{
+    public partial class AddModel : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Suerte",
+                columns: table => new
+                {
+                    FuturoId = table.Column<string>(type: "nvarchar(200)", maxLength: 200, nullable: false),
+                    Letra = table.Column<string>(type: "nvarchar(200)", maxLength: 200, nullable: false),
+                    Enlace = table.Column<string>(type: "nvarchar(1000)", maxLength: 1000, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Suerte", x => x.FuturoId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Suerte");
+        }
+    }
+}

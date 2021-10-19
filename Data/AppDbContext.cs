ad0d73d6433a5f6534be2c91ce9aa58f2f905a66
Juan daniel Gonzales Aramayo <a2020116081@estudiantes.upsa.edu.bo>
2021-10-18 22:45:26 -0400

    Cree el directorio Data con la classe AppDbContext

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApiSuerte.Models;
+
+namespace WebApiSuerte.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+
+        // Agregar los modelos a utilizar
+        public DbSet<Suerte> Suerte { get; set; }
+
+    }
+}

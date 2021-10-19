ef0cdcab7335cac71fe3197b1fd560f51ec759f2
Juan daniel Gonzales Aramayo <a2020116081@estudiantes.upsa.edu.bo>
2021-10-18 22:38:37 -0400

    Primer commit y Cree el modelo

---
+++ Models/Suerte.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApiSuerte.Models
+{
+    public class Suerte
+    {
+        [Key]
+        [StringLength(200, MinimumLength = 5, ErrorMessage = "La longitud de {0} debe estar entre {2} y {1}")]
+        public string FuturoId { get; set; }
+
+        [Required(AllowEmptyStrings = false, ErrorMessage = "{0} es requerido")]
+        [StringLength(200, MinimumLength = 5, ErrorMessage = "La longitud de {0} debe estar entre {2} y {1}")]
+        public string Letra { get; set; }
+
+        [Url]
+        [StringLength(1000, MinimumLength = 1, ErrorMessage = "La longitud de {0} debe estar entre {2} y {1}")]
+        public string Enlace { get; set; }
+
+
+    }
+}
